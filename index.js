c1380a317a9694f3625d86fd538020342d5652e7
shanmugappriyaa <pspri03@gmail.com>
2024-02-14 12:56:07 +0530

    CORS origin url added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "*",
+    origin: "https://shanmu-blog.netlify.app",
     methods: ["GET", "POST"],
     optionSuccessStatus: 200,
   })
